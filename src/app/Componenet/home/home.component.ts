b799691c2a94601773fa22b5df440d3accf0c574
xisxus <shefain3@gmail.com>
2024-09-16 09:51:51 +0600

    ng

--- src/app/Componenet/home/home.component.ts
+++ src/app/Componenet/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  navigateAndShowNav() {
+    // Example navigation
+    this.router.navigate(['/pro']);
+  }
+
+  showLoading() {
+    const overlay = document.getElementById('loading-overlay') as HTMLElement;
+    if (overlay) {
+      overlay.style.display = 'flex'; // Show the overlay
+
+     
+      setTimeout(() => {
+        overlay.style.display = 'none'; // Hide the overlay
+        window.location.href = '/some-path'; // Replace with your desired URL
+      }, 2000); // Adjust the timeout duration to match your loading animation
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ViewProductComponent } from './Componenet/view-product/view-product.component';
 import { EditProductComponent } from './Componenet/edit-product/edit-product.component';
 import { CreateProductComponent } from './Componenet/create-product/create-product.component';
+import { HomeComponent } from './Componenet/home/home.component';
 
 export const routes: Routes = [
     
-  {path: '' , component : ViewProductComponent},
+  {path: '' , component : HomeComponent},
+  {path: 'pro' , component : ViewProductComponent},
   // {path: 'product' , component : ViewProductComponent} ,
   {path: 'product/edit/:id' , component : EditProductComponent} ,
 
